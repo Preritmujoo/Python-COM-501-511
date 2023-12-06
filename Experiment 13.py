15008d59b6672df484e9753b20109cd0459f9e82
Prerit Mujoo <95234935+Preritmujoo@users.noreply.github.com>
2023-12-07 00:43:24 +0530

    Update Experiment 13.py

--- Experiment 13.py
+++ Experiment 13.py
@@ -1,17 +1,16 @@
+# AIM: Create a text file "MYFILE.TXT" in python and ask the user to write separate 3 lines with three input statements from the user.
+
 def extract_unique_words(file_name):
     unique_words = set()
 
     try:
         with open(file_name, 'r') as file:
             for line in file:
-                # Split the line into words
                 words = line.split()
 
-                # Remove punctuation and convert words to lowercase
                 words = [word.strip('.,!?()[]{}\'"') for word in words]
                 words = [word.lower() for word in words]
 
-                # Add unique words to the set
                 unique_words.update(words)
     except FileNotFoundError:
         print(f"File '{file_name}' not found.")
