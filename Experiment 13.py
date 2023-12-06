8f8b9c25eb8669eadf067592f8b35f8ad4208e02
Prerit Mujoo <95234935+Preritmujoo@users.noreply.github.com>
2023-12-07 00:40:18 +0530

    Create Experiment 13.py

---
+++ Experiment 13.py
@@ -0,0 +1,32 @@
+def extract_unique_words(file_name):
+    unique_words = set()
+
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                # Split the line into words
+                words = line.split()
+
+                # Remove punctuation and convert words to lowercase
+                words = [word.strip('.,!?()[]{}\'"') for word in words]
+                words = [word.lower() for word in words]
+
+                # Add unique words to the set
+                unique_words.update(words)
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+        return []
+
+    return sorted(unique_words)
+
+def main():
+    file_name = input("Enter the file name: ")
+    unique_words = extract_unique_words(file_name)
+
+    if unique_words:
+        print("Unique words in alphabetical order:")
+        for word in unique_words:
+            print(word)
+
+if _name_ == "_main_":
+    main()
